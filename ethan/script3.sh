41a6e7c1c6f1f3c74ed7901f9cbc8eb230b697c6
crazyguy106 <crazyguy106@gmail.com>
2021-08-24 05:12:23 -0400

    Changes to be committed: 	new file:   ethan/ipblack.txt 	new file:   ethan/ipwhite.txt 	new file:   ethan/script.s 	new file:   ethan/script1.sh 	new file:   ethan/script2.sh 	new file:   ethan/script3.sh 	new file:   ethan/script4.sh 	new file:   ethan/script5.sh 	new file:   ethan/scriptdif.txt
     Changes not staged for commit:
    	deleted:    ipblack.txt
    	deleted:    ipwhite.txt
    	deleted:    script.sh
    	deleted:    script1.sh
    	deleted:    script2.sh
    	deleted:    script3.sh
    	deleted:    script4.sh
    	deleted:    script5.sh
    	deleted:    scriptdif.txt

---
+++ ethan/script3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+file=$1
+Failed=$(cat $file | grep Failed | wc -l)
+ipno=$(cat $file | grep Failed | awk '{print $(NF-3)}' | sort | uniq | wc -l)
+Fuser=$(cat $file | grep Failed | grep -v invalid | awk '{print $(NF-5)}' | sort | uniq)
+invalid=$(cat $file | grep Invalid | awk '{print $8}' | wc -l)
+ipfail=$(cat $file | grep Failed | awk '{print $(NF-3)}' | sort | uniq -c | sort -g | tail -n 1 | awk '{print $2}')
+ipfailno=$(cat $file | grep Failed | awk '{print $(NF-3)}' | sort | uniq -c | sort -g | tail -n 1 | awk '{print $1}')
+country=$(whois $ipfail | grep -i country | head -n 1 | awk '{print $NF}')
+
+echo "This script is to analyse the following results:"
+
+echo "1. The number of Failed attempts made on his system is" $Failed
+echo "2. The number of IP addresses involved in the attacks is" $ipno
+echo "3. The valid users with failed attempts are:"$Fuser
+echo "4. The number of invalid user attempts is" $invalid
+echo "5. The IP address with the most failed attempts is "$ipfail "who failed "$ipfailno "times and is from "$country
